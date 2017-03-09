0cb75b30bdefc170f3dcf81b95382fa30e8424ef
scotteggs <scotteggs@gmail.com>
2017-03-09 13:43:29 -0500

    initial mapbox tiles and leaflet addition

---
+++ main.js
@@ -0,0 +1,8 @@
+var mymap = L.map('mapid').setView([40.777557, -73.977018], 15);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'scotteggs.o7614jl2',
+    accessToken: 'pk.eyJ1Ijoic2NvdHRlZ2dzIiwiYSI6ImNpaDZoZzhmdjBjMDZ1cWo5aGcyaXlteTkifQ.LZe0-IBRQmZ0PkQBsYIliw'
+}).addTo(mymap);
