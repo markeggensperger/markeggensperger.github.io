290d3607b397eaa86e16e889a644379fb9a1cdb5
scotteggs <scotteggs@gmail.com>
2017-03-09 14:40:24 -0500

    enable auto locate on load

--- main.js
+++ main.js
@@ -4,5 +4,15 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'scotteggs.o7614jl2',
-    accessToken: 'pk.eyJ1Ijoic2NvdHRlZ2dzIiwiYSI6ImNpaDZoZzhmdjBjMDZ1cWo5aGcyaXlteTkifQ.LZe0-IBRQmZ0PkQBsYIliw'
+    accessToken: 'pk.eyJ1Ijoic2NvdHRlZ2dzIiwiYSI6ImNqMDJxY2Z4bjA3cGsycWxzN2xxODI5YnIifQ.RIHnvvc_z7Ia_4hxv8NBBg'
 }).addTo(mymap);
+
+
+var circle = L.circle([40.777557, -73.977018], {
+    color: 'blue',
+    fillColor: '#99ccff',
+    fillOpacity: 0.5,
+    radius: 100
+}).addTo(mymap);
+
+mymap.locate({setView: true, maxZoom: 16});
