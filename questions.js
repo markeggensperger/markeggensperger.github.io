788f9e7cbd69cfc07f0e89af7bcd4e3862799e3c
scotteggs <scotteggs@gmail.com>
2017-03-12 16:23:27 -0400

    added question forms

---
+++ questions.js
@@ -0,0 +1,32 @@
+var nameInput = document.getElementById('question');
+
+
+document.querySelector('form').addEventListener('submit', function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+    console.log(nameInput.value)
+    if(nameInput.value == 'FS20') {
+        setImageVisible('clue4a', false)
+        setImageVisible('5abc', true)
+    }
+    if(nameInput.value == 'M027130') {
+        setImageVisible('clue4b', false)
+        setImageVisible('5abc', true)
+    }
+    if(nameInput.value == 'CHP55') {
+        setImageVisible('clue4c', false)
+        setImageVisible('5abc', true)
+    }
+    else {
+        alert("Try Again")
+    }
+
+});
+
+
+function setImageVisible(id, visible) {
+    var img = document.getElementById(id);
+    img.style.visibility = (visible ? 'visible' : 'hidden');
+    img.style.height = (visible ? '100%' : '0')
+}
